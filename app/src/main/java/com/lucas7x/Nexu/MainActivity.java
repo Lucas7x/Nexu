7e7e9ec5f2822f226240be2e006b76c094385b55
lucas xavier <lucassx1995@gmail.com>
2021-03-02 15:57:22 -0300

    Configuração inicial do projeto e dependências

---
+++ app/src/main/java/com/lucas7x/Nexu/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lucas7x.Nexu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
