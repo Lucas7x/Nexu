55812a82d4f89fd6e57bd81092388f6c84afaf5e
lucas xavier <lucassx1995@gmail.com>
2021-03-04 09:01:25 -0300

    interface login

--- app/src/main/java/com/lucas7x/Nexu/activity/LoginActivity.java
+++ app/src/main/java/com/lucas7x/Nexu/activity/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.lucas7x.Nexu;
+package com.lucas7x.Nexu.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.lucas7x.Nexu.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
