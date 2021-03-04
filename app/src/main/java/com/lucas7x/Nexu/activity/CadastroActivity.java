c4d42215d66a4f173cf2ae3fef4733aec748c1de
lucas xavier <lucassx1995@gmail.com>
2021-03-04 09:41:15 -0300

    interface cadastro

---
+++ app/src/main/java/com/lucas7x/Nexu/activity/CadastroActivity.java
@@ -0,0 +1,16 @@
+package com.lucas7x.Nexu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lucas7x.Nexu.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
--- app/src/main/java/com/lucas7x/Nexu/activity/LoginActivity.java
+++ app/src/main/java/com/lucas7x/Nexu/activity/LoginActivity.java
@@ -2,7 +2,9 @@ package com.lucas7x.Nexu.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.lucas7x.Nexu.R;
 
@@ -12,5 +14,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void abrirCadastro(View view) {
+        Intent i = new Intent(LoginActivity.this, CadastroActivity.class);
+        startActivity(i);
     }
 }
