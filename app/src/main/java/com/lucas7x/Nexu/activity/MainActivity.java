94c1c053e0f1dc6fdc659696908130d57b6ad8f6
lucas xavier <lucassx1995@gmail.com>
2021-03-10 09:56:27 -0300

    toolbar e logout

--- app/src/main/java/com/lucas7x/Nexu/activity/MainActivity.java
+++ app/src/main/java/com/lucas7x/Nexu/activity/MainActivity.java
@@ -1,16 +1,65 @@
 package com.lucas7x.Nexu.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.lucas7x.Nexu.R;
+import com.lucas7x.Nexu.helper.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //configurar a toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+
+        //configuração de objetos
+        autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+
+    } //fim do onCreate
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_sair:
+                deslogarUsuario();
+
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario() {
+        try {
+            autenticacao.signOut();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
